44611bdce7014512990750e38d9d6a13fdf3e6bf
Jack Danger Canty <gitcommit@6brand.com>
2011-04-05 15:56:00 -0700

    extracting delete_index and build_index methods

--- lib/tanker/utilities.rb
+++ lib/tanker/utilities.rb
@@ -9,24 +9,35 @@ module Tanker
         get_model_classes.map{|model| model.tanker_config.index_name}.uniq.compact
       end
 
+      def delete_index(index_name)
+        index = Tanker.api.get_index(index_name)
+
+        if index.exists?
+          puts "Deleting #{index_name} index"
+          index.delete_index
+        end
+      rescue => e
+        puts "There was an error clearing the #{index_name} index: #{e.to_s}"
+      end
+
+      def build_index(index_name)
+        index = Tanker.api.get_index(index_name)
+        return if index.exists?
+
+        puts "Creating #{index_name} index"
+        index.create_index
+        puts "Waiting for the index to be ready"
+        while not index.running?
+          sleep 0.5
+        end
+      rescue => e
+        puts "There was an error creating the #{index_name} index: #{e.to_s}"
+      end
+
       def clear_all_indexes
         get_available_indexes.each do |index_name|
-          begin
-            index = Tanker.api.get_index(index_name)
-
-            if index.exists?
-              puts "Deleting #{index_name} index"
-              index.delete_index()
-            end
-            puts "Creating #{index_name} index"
-            index.create_index()
-            puts "Waiting for the index to be ready"
-            while not index.running?
-              sleep 0.5
-            end
-          rescue => e
-            puts "There was an error clearing or creating the #{index_name} index: #{e.to_s}"
-          end
+          delete_index index_name
+          build_index  index_name
         end
       end
 
@@ -34,8 +45,9 @@ module Tanker
         get_model_classes.each do |klass|
           klass.tanker_reindex
         end
+      rescue => error
+        puts error.to_s
       end
     end
   end
 end
-
