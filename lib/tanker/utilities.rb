4936f84d4d76f433b406913a8458e9186e146dcc
Jack Danger Canty <gitcommit@6brand.com>
2011-05-16 22:01:41 -0700

    merging Dan's constantize method into utilities

--- lib/tanker/utilities.rb
+++ lib/tanker/utilities.rb
@@ -73,10 +73,16 @@ module Tanker
         end
       end
 
-      def constantize(klass_name)
-        Object.const_defined?(klass_name) ?
-                  Object.const_get(klass_name) :
-                  Object.const_missing(klass_name)
+      # borrowed from Rails' ActiveSupport::Inflector
+      def constantize(camel_cased_word)
+        names = camel_cased_word.split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
       end
 
       def detect_model(result)
